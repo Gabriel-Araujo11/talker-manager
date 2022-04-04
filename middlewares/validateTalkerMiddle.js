640e6d45143aaf5cfd75e6c9dbe9ceab5eaaa177
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2022-04-04 17:51:44 -0300

    feat(postTalkers): add validate rate, talk e watchedAt

--- middlewares/validateTalkerMiddle.js
+++ middlewares/validateTalkerMiddle.js
@@ -1,3 +1,5 @@
+const REGEX_DDMMAAAA = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/i;
+
 function validadeTalkerByName(name) {
     if (!name || name === '') {
         return { message: 'O campo "name" é obrigatório' };
@@ -18,4 +20,40 @@ function validadeTalkerByName(name) {
         }
     }
     
-module.exports = { validadeTalkerByName, validateTalkerByAge };
+    function validateWatchedAtRate(watchedAt, rate) {
+        if (!REGEX_DDMMAAAA.test(watchedAt)) { 
+            return { message: 'O campo "WatchedAt" deve ter o formato "dd/mm/aaaa"' };
+        }
+
+        if (!Number.isInteger(rate) || rate < 0 || rate > 5) {
+            // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+            return { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+          }
+        }
+
+    function validateTalk(talk) {
+        if (!talk || !talk.watchedAt || !talk.rate) {
+        return {
+             message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' };
+    }
+    const message = validateWatchedAtRate(talk.watchedAt, talk.rate);
+    if (message) {
+      return message;
+    }
+  }
+
+  function validateByAll(t) {
+      let messageFinal = validadeTalkerByName(t.name);
+      if (messageFinal) {
+          return messageFinal;
+      }
+
+      messageFinal = validateTalkerByAge(t.age);
+      if (messageFinal) {
+          return messageFinal;
+      }
+      messageFinal = validateTalk(t.talk);
+      return messageFinal;
+      }
+
+module.exports = { validateByAll };
